de2694fcc2c4b816e1718a5b63534316811f3731
Gakj <gabrielvilar88@gmail.com>
2024-11-12 13:30:04 -0300

    button changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function flipCard(button) {
     const poster = button.closest('.poster');
-    poster.classList.toggle('flipped');
+    poster.classList.toggle('flipped'); // Alterna a classe 'flipped' no cartaz específico
   }
