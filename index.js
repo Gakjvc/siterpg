da049e4a0e322530d49d255fb71333662245aa2a
Gakj <gabrielvilar88@gmail.com>
2024-11-22 10:02:47 -0300

    xp bar to bottom of card

--- index.js
+++ index.js
@@ -2,19 +2,24 @@ function flipCard(button) {
     const poster = button.closest('.poster');
     poster.classList.toggle('flipped'); // Alterna a classe 'flipped' no cartaz específico
   }
-  
-let currentProgress = 0;
-const progressBar = document.getElementById('progressBar');
-const progressText = document.getElementById('progressText');
 
-function updateProgress(value, target) {
-    currentProgress += value;
+  let currentProgress = 0;
+  const startValue = 300;
+  const targetValue = 900;
 
-    if (currentProgress > target) {
-        currentProgress = target;
-    }
+  const progressBar = document.getElementById('progressBar');
+  const currentValue = document.getElementById('currentValue');
 
-    const progressPercent = (currentProgress / target) * 100;
-    progressBar.style.width = progressPercent + '%';
-    progressText.textContent = `Progresso: ${Math.floor(progressPercent)}%`;
-}
+  function updateProgress(value, target) {
+      currentProgress += value;
+
+      if (currentProgress > target) {
+          currentProgress = target;
+      }
+
+      const progressPercent = (currentProgress / target) * 100;
+      const progressValue = Math.floor((startValue + (currentProgress / target) * (targetValue - startValue)));
+
+      progressBar.style.width = progressPercent + '%';
+      currentValue.textContent = progressValue;
+  }
