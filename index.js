cd51f1e2b0d1f05180b56215e4a2d77b82250aaf
Gakj <gabrielvilar88@gmail.com>
2024-11-22 09:01:32 -0300

    barra de xp

--- index.js
+++ index.js
@@ -2,3 +2,19 @@ function flipCard(button) {
     const poster = button.closest('.poster');
     poster.classList.toggle('flipped'); // Alterna a classe 'flipped' no cartaz específico
   }
+  
+let currentProgress = 0;
+const progressBar = document.getElementById('progressBar');
+const progressText = document.getElementById('progressText');
+
+function updateProgress(value, target) {
+    currentProgress += value;
+
+    if (currentProgress > target) {
+        currentProgress = target;
+    }
+
+    const progressPercent = (currentProgress / target) * 100;
+    progressBar.style.width = progressPercent + '%';
+    progressText.textContent = `Progresso: ${Math.floor(progressPercent)}%`;
+}
